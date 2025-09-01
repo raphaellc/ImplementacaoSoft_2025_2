3248efdb86283661b9d9500075251c998a71c7bf
RAPHAEL LEITE CAMPOS <raphaellc@users.noreply.github.com>
2025-09-01 16:23:18 -0300

    Criação do Banco e Tabela

--- HelloWorld/app/__init__.py
+++ HelloWorld/app/__init__.py
@@ -1,8 +1,26 @@
 from flask import Flask
 from .hello.routes import hello_bp
+from flask_sqlalchemy import SQLAlchemy
+
+
+
 
 def create_app():
     app = Flask(__name__)
     app.register_blueprint(hello_bp, url_prefix='/hello')
-    return app
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+    db = SQLAlchemy(app)
+
+    #definindo um modelo de dados
+    class User(db.Model):
+        id = db.Column(db.Integer, primary_key=True)
+        username = db.Column(db.String(80), unique=True, nullable=False)
+        email = db.Column(db.String(120), unique=True, nullable=False)
+        
+    with app.app_context():
+        db.create_all()
+
     
+
+    return app
+

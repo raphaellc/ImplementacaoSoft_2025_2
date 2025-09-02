1de72de469d51efa6c95ea47817e23bda3c12f71
RAPHAEL LEITE CAMPOS <raphaellc@users.noreply.github.com>
2025-09-02 18:41:23 -0300

    Lendo do Banco de Dados e mostrando

--- HelloWorld/app/hello/routes.py
+++ HelloWorld/app/hello/routes.py
@@ -1,7 +1,31 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request, redirect, url_for
+from ..models import User, db  # Importa a classe User do models.py
 
 hello_bp = Blueprint('hello', __name__, url_prefix='/hello')
 
 @hello_bp.route('/')
 def index():
-    return 'Hello, World!'
+    #usuarios = ['João', 'Maria', 'Pedro', 'Ana']
+    usuarios = User.query.all()
+    return render_template('index.html', usuarios=usuarios)
+
+@hello_bp.route('/novoUsuario', methods=['GET', 'POST'])
+def novoUsuario():
+    if request.method == 'POST':
+        # Obtém os dados do formulário
+        username = request.form['nome_usuario']
+        email = 'r@gmail.com'#request.form['email']
+
+        # Cria uma nova instância do modelo User
+        novo_usuario = User(username=username, email=email)
+
+        # Adiciona o novo usuário à sessão do banco de dados
+        db.session.add(novo_usuario)
+
+        # Salva as mudanças no banco de dados
+        db.session.commit()
+
+        # Redireciona para a página principal após a criação
+        return redirect('/hello')
+
+    return "Método não permitido", 405
